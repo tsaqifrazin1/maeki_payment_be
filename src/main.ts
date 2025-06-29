40bafff9d3587f1b038a612338c383bf9abf0e7a
tsaqifrazin <tsaqif.gojek@gmail.com>
2025-06-29 12:24:06 +0700

    remove cors

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
+  const app = await NestFactory.create(AppModule, { cors: true });
+
   // Enable CORS
-  app.enableCors({
-    origin: 'http://localhost:3002', // URL frontend
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
 }
